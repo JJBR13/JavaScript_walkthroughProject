7e2665039d00200c51a8211b4243510d63a61356
JJBR13 <jjbrown1308@gmail.com>
2022-08-23 13:19:36 +0000

    vid 5, random num 1-25 JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Waiting for DOM to finish loading before running the game
-// Get the button elements and adding event liserners
+// Get the button elements and adding event listeners
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -16,8 +16,16 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+/**
+ * The main game "loop*, called when script is first loaded
+ * and after the user's answer has been processed
+ */
+
 function runGame() {
 
+    // Generates 2 random number between 1 and 25
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
 }
 
 function checkAnswer() {
